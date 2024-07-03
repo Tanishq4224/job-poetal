5986cb10dcd83857ae10361b1a86d4a88755ee4b
Tanishq4224 <tanishq4224@gmail.com>
2024-07-03 19:39:20 +0530

    added navbar js

---
+++ script.js
@@ -0,0 +1,10 @@
+let navbar = document.querySelector('.header .flex .navbar')
+let menuBtn = document.querySelector('#menu-btn')
+
+let toggleMenu = e =>{
+    navbar.classList.toggle('active')
+} 
+menuBtn.addEventListener('click',toggleMenu)
+
+
+
