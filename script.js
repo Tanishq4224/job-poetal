04107e5cd025e783db3236037d0657b3b61b89a3
Tanishq4224 <tanishq4224@gmail.com>
2024-07-08 17:36:02 +0530

    bug fixes and patches

--- script.js
+++ script.js
@@ -9,3 +9,32 @@ menuBtn.addEventListener('click',toggleMenu)
 
 
 
+let dropdown_items = document.querySelectorAll('.dropdown p')
+let inputField = document.querySelectorAll('.output')
+
+
+
+dropdown_items.forEach (items =>{
+items.onclick = () =>{
+    items_parent = items.parentElement.parentElement;
+    let output = items_parent.querySelector('.output')
+    output.value = items.innerHTML
+}
+
+})
+
+
+
+let company = document.querySelectorAll('.company')
+let company_name = document.querySelectorAll('.company-name')
+let company_name_filter = document.querySelector("#company-name")
+
+let search_job = document.querySelector('.job-search')
+
+search_job.addEventListener('click', ()=>{
+    company.forEach(company = ()=>{
+       if (company_name_filter.value != company_name.innerText) {
+        console.log('hi')
+       }
+    })
+})
